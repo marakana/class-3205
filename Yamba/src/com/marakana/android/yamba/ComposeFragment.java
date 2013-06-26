eec2bcf89dc84799bb2ebbf51e4454fe694fb477
Ken Jones <ken.jones@marakana.com>
2013-06-26 12:38:43 -0700

    Refactored MainActivity to use a fragment

---
+++ Yamba/src/com/marakana/android/yamba/ComposeFragment.java
@@ -0,0 +1,99 @@
+package com.marakana.android.yamba;
+
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
+
+public class ComposeFragment extends Fragment {
+
+	private static final String TAG = "ComposeFragment";
+
+	private Button buttonSubmit;
+	private EditText editMsg;
+	private Toast toast;
+
+	private YambaClient client;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Create a YambaClient object
+		client = new YambaClient("student", "password");
+	}
+
+	@SuppressLint("ShowToast")
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_compose, container,
+				false);
+		// Look up the user text entry field
+		editMsg = (EditText) view.findViewById(R.id.edit_message);
+
+		// Register our button click listener
+		buttonSubmit = (Button) view.findViewById(R.id.button_submit);
+		buttonSubmit.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (BuildConfig.DEBUG)
+					Log.v(TAG, "Button clicked");
+				postStatus();
+			}
+		});
+
+		// Create and cache a Toast for reuse
+		toast = Toast.makeText(getActivity().getApplicationContext(), null,
+				Toast.LENGTH_LONG);
+
+		return view;
+	}
+
+	private void postStatus() {
+		// The Submit button was clicked
+		String msg = editMsg.getText().toString();
+		Log.i(TAG, "User entered: " + msg);
+
+		// Clear the EditText content
+		editMsg.setText("");
+
+		// Post the message -- as long as there is a message to post
+		if (!TextUtils.isEmpty(msg)) {
+			new PostStatusTask().execute(msg);
+		}
+	}
+
+	private class PostStatusTask extends AsyncTask<String, Void, Integer> {
+
+		@Override
+		protected Integer doInBackground(String... params) {
+			int resultMsg = R.string.post_status_fail;
+			try {
+				// Post the new status message
+				client.postStatus(params[0]);
+				resultMsg = R.string.post_status_success;
+			} catch (YambaClientException e) {
+				Log.e(TAG, "Unable to post status update", e);
+			}
+			return resultMsg;
+		}
+
+		@Override
+		protected void onPostExecute(Integer result) {
+			toast.setText(result);
+			toast.show();
+		}
+	}
+}
--- Yamba/src/com/marakana/android/yamba/MainActivity.java
+++ Yamba/src/com/marakana/android/yamba/MainActivity.java
@@ -1,139 +1,20 @@
 package com.marakana.android.yamba;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.marakana.android.yamba.clientlib.YambaClient;
-import com.marakana.android.yamba.clientlib.YambaClientException;
+public class MainActivity extends Activity {
 
-public class MainActivity extends Activity implements View.OnClickListener {
-
-    private static final String TAG = "MainActivity";
-    private Button buttonSubmit;
-    private EditText editMsg;
-    private Toast toast;
-    
-    private YambaClient client;
+	private static final String TAG = "MainActivity";
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (BuildConfig.DEBUG) Log.v(TAG, "onCreate() invoked");
-        
-        setContentView(R.layout.activity_main);
-        
-        // Look up the user text entry field
-        editMsg = (EditText) findViewById(R.id.edit_message);
-        
-        // Register our button click listener
-        buttonSubmit = (Button) findViewById(R.id.button_submit);
-        buttonSubmit.setOnClickListener( this );
-        
-/*        buttonSubmit.setOnClickListener( new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (BuildConfig.DEBUG) Log.v(TAG, "Button clicked");
-			}
-		});
-*/
-        
-        // Create and cache a Toast for reuse
-        toast = Toast.makeText(getApplicationContext(), null, Toast.LENGTH_LONG);
-        
-        // Create a YambaClient object
-        client = new YambaClient("student", "password");
-        }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (BuildConfig.DEBUG)
+			Log.v(TAG, "onCreate() invoked");
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-	    if (BuildConfig.DEBUG) Log.v(TAG, "onStart() invoked");
+		setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-	    if (BuildConfig.DEBUG) Log.v(TAG, "onResume() invoked");
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
-	    if (BuildConfig.DEBUG) Log.v(TAG, "onPause() invoked");
-	}
-
-	@Override
-	protected void onStop() {
-		super.onStop();
-	    if (BuildConfig.DEBUG) Log.v(TAG, "onStop() invoked");
-	}
-
-	@Override
-	protected void onRestart() {
-		super.onRestart();
-	    if (BuildConfig.DEBUG) Log.v(TAG, "onRestart() invoked");
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		if (BuildConfig.DEBUG) Log.v(TAG, "onDestroy() invoked");
-	}
-	
-	@Override
-	public void onClick(View v) {
-		if (BuildConfig.DEBUG) Log.v(TAG, "Button clicked");
-		int id = v.getId();
-		switch (id) {
-		case R.id.button_submit:
-			// The Submit button was clicked
-			String msg = editMsg.getText().toString();
-			Log.i(TAG, "User entered: " + msg);
-			
-			// Clear the EditText content
-			editMsg.setText("");
-			
-			// Post the message -- as long as there is a message to post
-			if (!TextUtils.isEmpty(msg)) {
-				new PostStatusTask().execute(msg);
-			}
-			
-			break;
-		default:
-			// Unknown button? We shouldn't be here!
-			Log.w(TAG, "An unknown button was clicked!");
-		}
-	}
-	
-	private class PostStatusTask extends AsyncTask<String, Void, Integer> {
-
-		@Override
-		protected Integer doInBackground(String... params) {
-			int resultMsg = R.string.post_status_fail;
-			try {
-				// Post the new status message
-				client.postStatus(params[0]);
-				resultMsg = R.string.post_status_success;
-			} catch (YambaClientException e) {
-				Log.e(TAG, "Unable to post status update", e);
-			}
-			return resultMsg;
-		}
-
-		@Override
-		protected void onPostExecute(Integer result) {
-			toast.setText(result);
-			toast.show();
-		}
-		
-	}
-
-    
 }
