92e494da8627ced4fc6b87f03c262ef54e4f43ef
Ken Jones <ken.jones@marakana.com>
2013-06-26 22:31:26 -0700

    Registered the application object as a preference change listener. Discard existing YambaClient whenever related preference values change.

--- Yamba/src/com/marakana/android/yamba/YambaApplication.java
+++ Yamba/src/com/marakana/android/yamba/YambaApplication.java
@@ -1,20 +1,67 @@
 package com.marakana.android.yamba;
 
-import com.marakana.android.yamba.clientlib.YambaClient;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
 
 public class YambaApplication extends Application {
 	
 	private static YambaClient yambaClient;
+	private static final Object clientLock = new Object();
+	
+	private static SharedPreferences prefs;
+	private static final String prefKeyUser = "PREF_KEY_USER";
+	private static final String prefKeyPassword = "PREF_KEY_PASSWORD";
+	private static final Set<String> yambaClientPrefKeys;
 
-	public static synchronized YambaClient getYambaClient() {
-		if (null == yambaClient) {
-			String user = "student";
-			String password = "password";
-			yambaClient = new YambaClient(user, password);
+	private static final String TAG = "YambaApplication";
+	
+	static {
+		Set<String> keys = new HashSet<String>();
+		keys.add(prefKeyUser);
+		keys.add(prefKeyPassword);
+		yambaClientPrefKeys = Collections.unmodifiableSet(keys);
+	}
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		prefs.registerOnSharedPreferenceChangeListener( new OnSharedPreferenceChangeListener() {
+			
+			@Override
+			public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+					String key) {
+				if (BuildConfig.DEBUG) Log.d(TAG, "Preference changed: " + key);
+				// If any of the preferences related to the YambaClient change,
+				// discard any existing YambaClient.
+				if (yambaClientPrefKeys.contains(key)) {
+					synchronized (clientLock) {
+						yambaClient = null;
+					}
+				}
+			}
+		});
+	}
+
+	public static YambaClient getYambaClient() {
+		synchronized (clientLock) {
+			if (null == yambaClient) {
+				// Create a YambaClient based on current preference values.
+				String user = prefs.getString(prefKeyUser, null);
+				String password = prefs.getString(prefKeyPassword, null);
+				yambaClient = new YambaClient(user, password);
+			}
+			return yambaClient;
 		}
-		return yambaClient;
 	}
 
 }
